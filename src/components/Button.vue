6cfab7de75c592ee5b1a91a3ef470c8192049863
imeepablo <imeegil@gmail.com>
2022-11-08 16:09:57 +0800

    Create task tracker app

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button 
+        class="btn"
+        :style="{background:color}"
+        @click="onClick"
+    >
+        {{text}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'VButton',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick() {
+         this.$emit('btn-click')
+        }
+    }
+}
+</script>
